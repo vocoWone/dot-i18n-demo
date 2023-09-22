f20cb71d77d0b086bc42e1950c2d968f527da6ec
vocoWone <wangzh@tuya.com>
2023-09-22 14:45:32 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
+// App.tsx
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+return (
     <div className="App">
-      <header className="App-header">
+    <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <i18n>我是xml形式多语言词条，性能更优</i18n>
+        </div>
+        <div>
+            {i18n("我是函数式多语言词条，使用更灵活")}
+        </div>
+        -------------------------------------------
+        <div>
+            <i18n namespace="my">我有自己的命名空间</i18n>
+        </div>
+        <div>
+            {i18n("我也有自己的命名空间", {"namespace": "ym"})}
+        </div>
+        -------------------------------------------
+        <div>
+            <i18n>我不支持变量</i18n>
+        </div>
+        <div>
+            {i18n("但是我支持变量{value}", {replace: {"{value}": i18n("--新值--")}})}
+        </div>
+    </header>
     </div>
-  );
+);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import DotI18N from "dot-i18n";
+import locales from "./locales";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const Inex = () => {
+  const [lang, setLang] = useState<"zh" | "en">("zh");
+  const changeEn = () => setLang("en")
+  const changeZh = () => setLang("zh");
+
+  return (
+    <DotI18N.Provider locales={locales[lang]}>
+      <button onClick={changeEn}>切换为英文</button>
+      <button onClick={changeZh}>切换为中文</button>
+      <App />
+    </DotI18N.Provider>
+  )
+}
+
+root.render(<Inex />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
